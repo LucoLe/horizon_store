85651f38ba6432a4b5899176f7b0c8dc16f540f5
Lyudmil Lesinski <lyudmilles@gmail.com>
2022-10-21 15:22:20 +0200

    Initial commit

---
+++ lib/horizon_store.ex
@@ -0,0 +1,18 @@
+defmodule HorizonStore do
+  @moduledoc """
+  Documentation for `HorizonStore`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HorizonStore.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/horizon_store_test.exs
@@ -0,0 +1,8 @@
+defmodule HorizonStoreTest do
+  use ExUnit.Case
+  doctest HorizonStore
+
+  test "greets the world" do
+    assert HorizonStore.hello() == :world
+  end
+end
